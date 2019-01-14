b981e0bfcd9f0bff2425c5ae3d493af72d4a5c1a
y_tsubasa <tbs_chako@yahoo.co.jp>
2019-01-14 17:46:23 +0900

    【順位表】予備日などで日程情報にチーム名が入ってない場合に落ちる事象を解消

--- app/models/event_info.rb
+++ app/models/event_info.rb
@@ -7,27 +7,27 @@ class EventInfo < ApplicationRecord
     event_info = EventInfo.find_by(year: year, season: season, league: league)
     team_list = []
     event_info.event_one_days.each do |event_one_day|
-      unless team_list.include?(event_one_day.top_team_1)
+      unless team_list.include?(event_one_day.top_team_1) || event_one_day.top_team_1.blank?
         team_list << event_one_day.top_team_1
       end
 
-      unless team_list.include?(event_one_day.bottom_team_1)
+      unless team_list.include?(event_one_day.bottom_team_1) || event_one_day.bottom_team_1.blank?
         team_list << event_one_day.bottom_team_1
       end
 
-      unless team_list.include?(event_one_day.top_team_2)
+      unless team_list.include?(event_one_day.top_team_2) || event_one_day.top_team_2.blank?
         team_list << event_one_day.top_team_2
       end
 
-      unless team_list.include?(event_one_day.bottom_team_2)
+      unless team_list.include?(event_one_day.bottom_team_2) || event_one_day.bottom_team_2.blank?
         team_list << event_one_day.bottom_team_2
       end
 
-      unless team_list.include?(event_one_day.top_team_3)
+      unless team_list.include?(event_one_day.top_team_3) || event_one_day.top_team_3.blank?
         team_list << event_one_day.top_team_3
       end
 
-      unless team_list.include?(event_one_day.bottom_team_3)
+      unless team_list.include?(event_one_day.bottom_team_3) || event_one_day.bottom_team_3.blank?
         team_list << event_one_day.bottom_team_3
       end
     end

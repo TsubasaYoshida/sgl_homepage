c667a930d403457371d686fda1142647dd969d90
y_tsubasa <tbs_chako@yahoo.co.jp>
2019-02-28 17:15:34 +0900

    add JavaScript about auto-setting-start-time

--- app/assets/javascripts/round_select.js
+++ app/assets/javascripts/round_select.js
@@ -24,3 +24,24 @@ function auto_select_round(round_1_select) {
         }
     }
 }
+
+function auto_setting_start_time(start_time_1_input) {
+    var start_time_1 = start_time_1_input.value;
+    var [h, m] = start_time_1.split(':');
+
+    var date = new Date();
+    date.setHours(h);
+    date.setMinutes(m);
+    date.setMinutes(date.getMinutes() + 150);
+
+    var id_1 = start_time_1_input.id;
+    var id_2 = id_1.replace('start_time_1', 'start_time_2');
+    var start_time_2_input = document.getElementById(id_2);
+    start_time_2_input.value = ('0' + date.getHours()).slice(-2) + ':' + ('0' + date.getMinutes()).slice(-2);
+
+    date.setMinutes(date.getMinutes() + 150);
+
+    var id_3 = id_1.replace('start_time_1', 'start_time_3');
+    var start_time_3_input = document.getElementById(id_3);
+    start_time_3_input.value = ('0' + date.getHours()).slice(-2) + ':' + ('0' + date.getMinutes()).slice(-2);
+}

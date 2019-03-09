45ca2fc7f4ad8d5c1a2728503eb7adb3c2197be9
y_tsubasa <tbs_chako@yahoo.co.jp>
2019-03-09 18:36:32 +0900

    fix deploy.rb about database.yml

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,7 +14,7 @@ set :branch, 'master'
 set :deploy_to, '/var/www/sgl_homepage'
 
 # シンボリックリンクをはるファイル
-set :linked_files, fetch(:linked_files, []).push('config/secrets.yml')
+set :linked_files, fetch(:linked_files, []).push('config/secrets.yml', 'config/database.yml')
 
 # シンボリックリンクをはるフォルダ
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')

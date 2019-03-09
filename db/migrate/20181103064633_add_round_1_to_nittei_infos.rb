ef26ab56f02e0b3a1ae6fd3ad44e69603b4a42d3
y_tsubasa <tbs_chako@yahoo.co.jp>
2019-03-09 18:54:03 +0900

    delete migration-file about nittei_infos-table

--- db/migrate/20181103064633_add_round_1_to_nittei_infos.rb
+++
@@ -1,7 +0,0 @@
-class AddRound1ToNitteiInfos < ActiveRecord::Migration[5.2]
-  def change
-    add_column :nittei_infos, :round_1, :string
-    add_column :nittei_infos, :round_2, :string
-    add_column :nittei_infos, :round_3, :string
-  end
-end
--- db/migrate/20181210134953_drop_nittei_infos.rb
+++
@@ -1,5 +0,0 @@
-class DropNitteiInfos < ActiveRecord::Migration[5.2]
-  def change
-    drop_table :nittei_infos
-  end
-end

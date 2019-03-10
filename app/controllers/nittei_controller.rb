e4f6cc7051d454464bc0034695bd2d7ac28f449b
y_tsubasa <tbs_chako@yahoo.co.jp>
2019-03-10 14:16:14 +0900

    minute adjustment

--- app/controllers/nittei_controller.rb
+++ app/controllers/nittei_controller.rb
@@ -14,15 +14,25 @@ class NitteiController < ApplicationController
     else
       event_info = EventInfo.find_by(year: key_year, season: key_season, league: key_event)
 
-      # 同じ日に異なるラウンドが入った時にどうするか
-      @nittei_infos_1 = event_info.event_one_days.where(round_1: '第一節').order(disp_date: :asc)
-      @nittei_infos_2 = event_info.event_one_days.where(round_1: '第二節').order(disp_date: :asc)
+      if event_info
+        # 同じ日に異なるラウンドが入った時にどうするか
+        @nittei_infos_1 = event_info.event_one_days.where(round_1: '第一節').order(disp_date: :asc)
+        @nittei_infos_2 = event_info.event_one_days.where(round_1: '第二節').order(disp_date: :asc)
+        @oc_date = event_info.oc_date
+        @oc_time = event_info.oc_time
+
+        # 最終更新日時取得
+        first_date = event_info.event_one_days.order(updated_at: :desc).first
+        unless first_date.nil?
+          @updated_at = first_date.updated_at
+        end
+      else
+        @error_msg = '該当の情報はございません。'
+      end
 
       @selected_year = key_year
       @selected_season = key_season
       @selected_event = key_event
-      @oc_date = event_info.oc_date
-      @oc_time = event_info.oc_time
       render 'nittei/show'
     end
   end
@@ -41,6 +51,12 @@ class NitteiController < ApplicationController
     # 同じ日に異なるラウンドが入った時にどうするか
     @nittei_infos_1 = event_info.event_one_days.where(round_1: '第一節').order(disp_date: :asc)
     @nittei_infos_2 = event_info.event_one_days.where(round_1: '第二節').order(disp_date: :asc)
+
+    # 最終更新日時取得
+    first_date = event_info.event_one_days.order(updated_at: :desc).first
+    unless first_date.nil?
+      @updated_at = first_date.updated_at
+    end
 
     render 'nittei/show'
   end

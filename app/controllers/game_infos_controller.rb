cc7d3fd049c29ee17108c4b9b09649a7c17d3815
y_tsubasa <tbs_chako@yahoo.co.jp>
2019-03-12 23:20:08 +0900

    refactor of controller

--- app/controllers/game_infos_controller.rb
+++ app/controllers/game_infos_controller.rb
@@ -3,8 +3,6 @@ class GameInfosController < ApplicationController
   before_action :set_game_info, only: [:show, :edit, :score, :update, :destroy]
   layout 'admin', :except => [:index, :show, :narrow]
 
-  # GET /game_infos
-  # GET /game_infos.json
   def index
     @game_infos = GameInfo.all.order(disp_date: :desc, gameset_flag: :asc, number: :desc)
   end
@@ -27,70 +25,47 @@ class GameInfosController < ApplicationController
       @selected_season = key_season
       @selected_event = key_event
     end
-    render 'game_infos/index'
+    render :index
   end
 
-  # GET /game_infos/1
-  # GET /game_infos/1.json
   def show
   end
 
-  # GET /game_infos/new
   def new
     @game_info = GameInfo.new
   end
 
-  # GET /game_infos/1/edit
   def edit
   end
 
-  # GET /game_infos/1/score
   def score
   end
 
-  # GET /game_infos/management
   def management
     @game_infos = GameInfo.all.order(disp_date: :desc, gameset_flag: :asc, number: :desc)
   end
 
-  # POST /game_infos
-  # POST /game_infos.json
   def create
     @game_info = GameInfo.new(game_info_params)
 
-    respond_to do |format|
-      if @game_info.save
-        format.html {redirect_to '/game_infos/management', notice: '試合情報の作成に成功しました。'}
-        format.json {render :show, status: :created, location: @game_info}
-      else
-        format.html {render :new}
-        format.json {render json: @game_info.errors, status: :unprocessable_entity}
-      end
+    if @game_info.save
+      redirect_to action: :management, notice: '試合情報の作成に成功しました。'
+    else
+      render :new
     end
   end
 
-  # PATCH/PUT /game_infos/1
-  # PATCH/PUT /game_infos/1.json
   def update
-    respond_to do |format|
-      if @game_info.update(game_info_params)
-        format.html {redirect_to '/game_infos/management', notice: '試合情報の更新に成功しました。'}
-        format.json {render :show, status: :ok, location: @game_info}
-      else
-        format.html {render :edit}
-        format.json {render json: @game_info.errors, status: :unprocessable_entity}
-      end
+    if @game_info.update(game_info_params)
+      redirect_to action: :management, notice: '試合情報の更新に成功しました。'
+    else
+      render :edit
     end
   end
 
-  # DELETE /game_infos/1
-  # DELETE /game_infos/1.json
   def destroy
     @game_info.destroy
-    respond_to do |format|
-      format.html {redirect_to '/game_infos/management', notice: '試合情報の削除に成功しました。'}
-      format.json {head :no_content}
-    end
+    redirect_to action: :management, notice: '試合情報の削除に成功しました。'
   end
 
   private
@@ -102,6 +77,16 @@ class GameInfosController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def game_info_params
-    params.require(:game_info).permit(:season, :event, :round, :disp_date, :batting_first_team, :field_first_team, :stadium, :number, :start_time, :end_time, :pitcher_top, :pitcher_bottom, :catcher_top, :catcher_bottom, :homerun_top, :homerun_bottom, :threebase_top, :threebase_bottom, :twobase_top, :twobase_bottom, :message, :top1, :bottom1, :top2, :bottom2, :top3, :bottom3, :top4, :bottom4, :top5, :bottom5, :top6, :bottom6, :top7, :bottom7, :top8, :bottom8, :top9, :bottom9, :top10, :bottom10, :top11, :bottom11, :top12, :bottom12, :top13, :bottom13, :top14, :bottom14, :top15, :bottom15, :gameset_flag)
+    params.require(:game_info).permit(:season, :event, :round, :disp_date,
+                                      :batting_first_team, :field_first_team,
+                                      :stadium, :number, :start_time, :end_time,
+                                      :pitcher_top, :pitcher_bottom, :catcher_top, :catcher_bottom,
+                                      :homerun_top, :homerun_bottom, :threebase_top, :threebase_bottom,
+                                      :twobase_top, :twobase_bottom, :message,
+                                      :top1, :bottom1, :top2, :bottom2, :top3, :bottom3, :top4, :bottom4,
+                                      :top5, :bottom5, :top6, :bottom6, :top7, :bottom7, :top8, :bottom8,
+                                      :top9, :bottom9, :top10, :bottom10, :top11, :bottom11, :top12, :bottom12,
+                                      :top13, :bottom13, :top14, :bottom14, :top15, :bottom15,
+                                      :gameset_flag)
   end
 end
--- app/controllers/infos_controller.rb
+++ app/controllers/infos_controller.rb
@@ -3,79 +3,53 @@ class InfosController < ApplicationController
   before_action :set_info, only: [:show, :edit, :update, :destroy]
   layout 'admin', :except => [:index, :show]
 
-  # GET /infos
-  # GET /infos.json
   def index
     @infos = Info.all.order(disp_date: :desc)
   end
 
-  # GET /infos/1
-  # GET /infos/1.json
   def show
   end
 
-  # GET /infos/new
   def new
     @info = Info.new
   end
 
-  # GET /infos/1/edit
   def edit
   end
 
-  # GET /infos/management
   def management
     @infos = Info.all.order(disp_date: :desc)
   end
 
-  # POST /infos
-  # POST /infos.json
   def create
     @info = Info.new(info_params)
 
-    respond_to do |format|
-      if @info.save
-        format.html {redirect_to '/infos/management', notice: '新着情報の作成に成功しました。'}
-        format.json {render :show, status: :created, location: @info}
-      else
-        format.html {render :new}
-        format.json {render json: @info.errors, status: :unprocessable_entity}
-      end
+    if @info.save
+      redirect_to action: :management, notice: '新着情報の作成に成功しました。'
+    else
+      render :new
     end
   end
 
-  # PATCH/PUT /infos/1
-  # PATCH/PUT /infos/1.json
   def update
-    respond_to do |format|
-      if @info.update(info_params)
-        format.html {redirect_to '/infos/management', notice: '新着情報の更新に成功しました。'}
-        format.json {render :show, status: :ok, location: @info}
-      else
-        format.html {render :edit}
-        format.json {render json: @info.errors, status: :unprocessable_entity}
-      end
+    if @info.update(info_params)
+      redirect_to action: :management, notice: '新着情報の更新に成功しました。'
+    else
+      render :edit
     end
   end
 
-  # DELETE /infos/1
-  # DELETE /infos/1.json
   def destroy
     @info.destroy
-    respond_to do |format|
-      format.html {redirect_to '/infos/management', notice: '新着情報の削除に成功しました。'}
-      format.json {head :no_content}
-    end
+    redirect_to action: :management, notice: '新着情報の削除に成功しました。'
   end
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_info
     @info = Info.find(params[:id])
   end
 
-  # Never trust parameters from the scary internet, only allow the white list through.
   def info_params
     params.require(:info).permit(:article, :disp_date)
   end

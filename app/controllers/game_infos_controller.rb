68efece085693bce1c41c4795c0477f1573d88cc
y_tsubasa <tbs_chako@yahoo.co.jp>
2019-04-01 23:36:35 +0900

    add-pagination-logic

--- app/controllers/game_infos_controller.rb
+++ app/controllers/game_infos_controller.rb
@@ -1,10 +1,20 @@
 class GameInfosController < ApplicationController
   skip_before_action :check_logined, only: [:index, :show, :narrow, :set_game_info]
   before_action :set_game_info, only: [:show, :edit, :score, :update, :destroy]
-  layout 'admin', :except => [:index, :show, :narrow]
+  layout 'admin', :except => [:index, :show, :narrow, :page]
+
+  PAGE_SIZE = 15
 
   def index
-    @game_infos = GameInfo.all.standard
+    @game_infos = GameInfo.all.standard.limit(PAGE_SIZE)
+    @games_count = (GameInfo.all.standard.size.to_f / PAGE_SIZE).ceil
+  end
+
+  def page
+    game_infos = GameInfo.all.standard
+    @game_infos = page_common(game_infos)
+    @games_count = (game_infos.size.to_f / PAGE_SIZE).ceil
+    render :index
   end
 
   def narrow
@@ -14,13 +24,15 @@ class GameInfosController < ApplicationController
     if key_year.nil? || key_season.nil? || key_event.nil?
       @error_msg = '該当の情報はございません。'
     else
-      @game_infos = GameInfo
-                        .where(season: key_season, event: key_event)
-                        .where('disp_date LIKE ?', "#{key_year}%")
-                        .order(disp_date: :desc, created_at: :desc)
-      if @game_infos.length == 0
+      game_infos = GameInfo
+                       .where(season: key_season, event: key_event)
+                       .where('disp_date LIKE ?', "#{key_year}%")
+                       .order(disp_date: :desc, created_at: :desc)
+      if game_infos.length == 0
         @error_msg = '該当の情報はございません。'
       end
+      @game_infos = page_common(game_infos)
+      @games_count = (game_infos.size.to_f / PAGE_SIZE).ceil
       @selected_year = key_year
       @selected_season = key_season
       @selected_event = key_event
@@ -69,6 +81,11 @@ class GameInfosController < ApplicationController
   end
 
   private
+
+  def page_common(game_infos)
+    page_num = params[:id] == nil ? 0 : params[:id].to_i - 1
+    game_infos.limit(PAGE_SIZE).offset(PAGE_SIZE * page_num)
+  end
 
   def set_game_info
     @game_info = GameInfo.find(params[:id])

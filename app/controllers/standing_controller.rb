85e54fcb448ba066687396815da76498f41aab46
y_tsubasa <tbs_chako@yahoo.co.jp>
2019-04-19 23:55:02 +0900

    refactor standings_controller

--- app/controllers/standing_controller.rb
+++ app/controllers/standing_controller.rb
@@ -85,7 +85,7 @@ class StandingController < ApplicationController
     @selected_year = GameInfo.order(disp_date: :desc).first.disp_date.year
     @selected_season = GameInfo.order(disp_date: :desc).first.season
 
-    if @selected_year.to_i <= 2018
+    if @selected_year.to_i <= 2017
       english_season = Constants.get_english_season(@selected_season)
       file_name = @selected_year.to_s + '_' + english_season
       render "standing/#{file_name}"
@@ -104,7 +104,7 @@ class StandingController < ApplicationController
       # 当時のチーム名一覧を取得（入替で所属リーグが変動するためteamsテーブルからは取得できない）
       team_name_list = EventInfo.get_team_name_list(@selected_year, @selected_season, event)
 
-      # チーム情報を設定
+      # チーム情報、チーム成績を設定
       record_list = []
       team_name_list.each do |team_name|
         team = Team.find_by(name: team_name)
@@ -113,88 +113,72 @@ class StandingController < ApplicationController
         record.three_letter_name = team.three_letter_name
         record.two_letter_name = team.two_letter_name
         record.one_letter_name = team.one_letter_name
-        record_list << record
-      end
 
-      record_list.each do |win_lose_draw|
-        team_name = win_lose_draw.team_name
         game_infos.each do |game_info|
           if team_name == game_info.batting_first_team || team_name == game_info.field_first_team
 
             if game_info.round != '順位決定戦'
               result = GameInfo.win_or_lose(game_info, team_name)
-
-              if result == :win
-                win_lose_draw.disp_win += 1
-              elsif result == :lose
-                win_lose_draw.disp_lose += 1
-              elsif result == :draw
-                win_lose_draw.disp_draw += 1
-              end
-
+              record.disp_win += 1 if result == :win
+              record.disp_lose += 1 if result == :lose
+              record.disp_draw += 1 if result == :draw
             else
               # 順位決定戦だけで勝敗判定メソッド呼び出し
               result_playoff = GameInfo.win_or_lose(game_info, team_name)
-
-              if result_playoff == :win
-                win_lose_draw.playoff_win += 1
-              elsif result_playoff == :lose
-                win_lose_draw.playoff_lose += 1
-              elsif result_playoff == :draw
-                win_lose_draw.playoff_draw += 1
-              end
-
+              record.playoff_win += 1 if result_playoff == :win
+              record.playoff_lose += 1 if result_playoff == :lose
+              record.playoff_draw += 1 if result_playoff == :draw
             end
-
           end
         end
 
         # 順位決定用のポイント計算
-        win_lose_draw.point = win_lose_draw.disp_win - win_lose_draw.disp_lose
-        win_lose_draw.playoff_point = win_lose_draw.playoff_win - win_lose_draw.playoff_lose
+        record.point = record.disp_win - record.disp_lose
+        record.playoff_point = record.playoff_win - record.playoff_lose
+
+        record_list << record
       end
 
       # 順位決定ロジック
-      rank_infos = record_list.sort_by do |a|
+      record_list.sort_by! do |a|
         [-a.point, -a.playoff_point]
       end
-      base_point = rank_infos.first.point
-      base_playoff_point = rank_infos.first.playoff_point
+      base_point = record_list.first.point
+      base_playoff_point = record_list.first.playoff_point
       base_rank = 1
-      rank_infos.each do |rank_info|
-        if base_point == rank_info.point && base_playoff_point == rank_info.playoff_point
-          rank_info.rank = base_rank
+      record_list.each do |record|
+        if base_point == record.point && base_playoff_point == record.playoff_point
+          record.rank = base_rank
         else
-          rank_info.rank = rank_infos.find_index(rank_info) + 1
-          base_rank = rank_info.rank
-          base_point = rank_info.point
-          base_playoff_point = rank_info.playoff_point
+          record.rank = record_list.find_index(record) + 1
+          base_rank = record.rank
+          base_point = record.point
+          base_playoff_point = record.playoff_point
         end
       end
 
       # ◯×△（ハイフンと空文字も）の生成
       result_array = []
-      rank_infos.each do |rank_info1|
-        rank_infos.each do |rank_info2|
-          if rank_info1.team_name == rank_info2.team_name
+      record_list.each do |record_1|
+        record_list.each do |record_2|
+          if record_1.team_name == record_2.team_name
             result_array << 'ー'
             result_array << 'ー'
           else
-            result_array << get_disp_result(game_infos, rank_info1.team_name, rank_info2.team_name, '第一節')
-            result_array << get_disp_result(game_infos, rank_info1.team_name, rank_info2.team_name, '第二節')
+            result_array << get_disp_result(game_infos, record_1.team_name, record_2.team_name, '第一節')
+            result_array << get_disp_result(game_infos, record_1.team_name, record_2.team_name, '第二節')
           end
         end
       end
 
       # 最終更新日時取得
       first_game_info = game_infos.order(updated_at: :desc).first
-      unless first_game_info.nil?
-        updated_at = first_game_info.updated_at
-      end
+      updated_at = first_game_info.updated_at if first_game_info
 
+      # 順位決定戦情報取得
       playoff_results = game_infos.where(round: '順位決定戦')
 
-      result_info = [rank_infos, result_array, updated_at, playoff_results]
+      result_info = [record_list, result_array, updated_at, playoff_results]
       @result_infos << result_info
     end
 

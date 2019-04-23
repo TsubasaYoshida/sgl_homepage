64cbd2975093048890f5342a76f98acb4f90d784
y_tsubasa <tbs_chako@yahoo.co.jp>
2019-04-24 00:12:42 +0900

    pass the test of games_controller

--- spec/factories/games.rb
+++ spec/factories/games.rb
@@ -1,8 +1,12 @@
 FactoryBot.define do
   factory :game, class: GameInfo do
-    batting_first_team {'先攻チーム'}
-    field_first_team {'後攻チーム'}
+    batting_first_team {'星槎道都大学'}
+    field_first_team {'札幌大谷大学'}
     disp_date {Time.zone.local(2019, 4, 30)}
+    season {'春季'}
+    event {'1部リーグ戦'}
+    round {'第一節'}
+    stadium {'札幌円山球場'}
     gameset_flag {false}
     number {3}
   end

7aa37cbaab1d4602b59033d1949eb7347db25fd5
y_tsubasa <tsubasa.yoshida.business@gmail.com>
2020-10-11 20:01:20 +0900

    2020年度代表決定戦の中止に伴って実装を変更する

--- app/models/championship.rb
+++ app/models/championship.rb
@@ -3,10 +3,13 @@ class Championship < ApplicationRecord
   has_many :championship_awards, dependent: :destroy
 
   def self.years_for_select
-    self.order(year: :desc).pluck(:year).map do |year|
+    years_for_select = self.order(year: :desc).pluck(:year).map do |year|
       [
         "第#{year.to_i - 2008}回(#{year})", year
       ]
     end
+    index = years_for_select.find_index { |pair| pair[1].to_i == 2020 }
+    years_for_select[index] = %w[王座決定戦(2020) 2020]
+    years_for_select
   end
 end

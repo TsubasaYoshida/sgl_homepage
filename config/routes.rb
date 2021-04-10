c4d4b462202cb6c4a5c1b5ab6154d39ab0653533
Tsubasa Yoshida <bassar@sonicgarden.jp>
2021-04-10 20:09:13 +0900

    fix: 実施条件・特別規定をアップデート

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   get 'standings' => 'standings#index'
   get 'admin' => 'admin#top'
   get 'stop_covid19' => 'static_pages#stop_covid19'
-  get 'special_rules_2020_autumn' => 'static_pages#special_rules_2020_autumn'
+  get 'special_rules_2021_spring' => 'static_pages#special_rules_2021_spring'
   get 'articles/1' => 'static_pages#replacement_battle_2020_autumn'
 
   get 'login' => 'login#index'
